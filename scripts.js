3984b1c81506dae5e7888633a245ecf7d1679266
Josh Broughton <josh.broughton47@gmail.com>
2022-09-15 15:17:23 -0600

    converted for loop to forEach

--- scripts.js
+++ scripts.js
@@ -2,24 +2,24 @@ import data from './data.js';
 
 const itemsContainer = document.querySelector('#items');
 
-for (let i = 0; i < data.length; i++) {
+data.forEach(function (item, i) {
     const newDiv = document.createElement('div');
     newDiv.className = 'item';
 
     const img = document.createElement('img');
-    img.src = data[i].image;
+    img.src = item.image;
     img.width = 300;
     img.height = 300;
     
     const description = document.createElement('p');
     const price = document.createElement('p');
 
-    description.innerText = data[i].desc;
-    price.innerText = data[i].price;
+    description.innerText = item.desc;
+    price.innerText = item.price;
 
     newDiv.appendChild(img);
     newDiv.appendChild(description);
     newDiv.appendChild(price);
 
     itemsContainer.appendChild(newDiv);
-}
+});
