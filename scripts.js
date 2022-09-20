6fe0a624aecd3ccd521b83de1e105e1436f6c896
Josh Broughton <josh.broughton47@gmail.com>
2022-09-20 12:45:28 -0600

    displayed cart items on console, ready to merge to main

--- scripts.js
+++ scripts.js
@@ -33,16 +33,55 @@ data.forEach(function (item, i) {
 const cart = [];
 
 function addItem(name, price) {
-    const item = {name: name, price: price, qty: 1};
+    for (let i = 0; i < cart.length; i++) {
+        if (cart[i].name === name) {
+            cart[i].qty += 1;
+            return
+        }
+    }
+    const item = {name, price, qty: 1};
     cart.push(item);
 }
 
+//print out the total number of items, the items in the cart, and the total cost
+//of items in the cart
 function showItems() {
-    console.log(cart);
+    console.log(`You have ${getQty()} items in your cart`);
+
+    for (let i = 0; i < cart.length; i++) {
+        console.log(`- ${cart[i].name} $${cart[i].price} x ${cart[i].qty}`);
+    }
+
+    
+    console.log(`The total cost is: $${getTotalPrice()}`);
+}
+
+//get total quantity of items in the cart
+function getQty() {
+    let totalQty = 0;
+
+    for (let i = 0; i < cart.length; i++) {
+        totalQty += cart[i].qty;
+    }
+
+    return totalQty
+}
+
+// get total price of items in cart
+function getTotalPrice() {
+    let totalPrice = 0;
+
+    for (let i =0; i < cart.length; i++) {
+        totalPrice += cart[i].price * cart[i].qty;
+    }
+
+    return totalPrice.toFixed(2);
 }
 
 addItem('Apple', 0.99);
 addItem('Orange', 1.29);
 addItem('Opinion', 0.02);
+addItem('Fribsee', 9.92);
+addItem('Apple', 0.99);
 
 showItems();
